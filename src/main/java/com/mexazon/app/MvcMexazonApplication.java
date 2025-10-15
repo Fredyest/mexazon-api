01af365ad7bba58c147335e5764eba12cc40b8f4
Freddy Rmz <alfredoro000@gmail.com>
2025-10-15 10:08:50 -0600

    JavaProject Mexazon

---
+++ src/main/java/com/mexazon/app/MvcMexazonApplication.java
@@ -0,0 +1,13 @@
+package com.mexazon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcMexazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcMexazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mexazon/app/MvcMexazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mexazon.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcMexazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
