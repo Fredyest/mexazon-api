875f8d2ba1e7ab80cc87bf9357a78bdf4bcb9d81
jakoobo90 <124805780+jakoobo90@users.noreply.github.com>
2025-10-16 20:02:06 -0600

    addiero los attributos de lo que pediste frediyin

--- src/main/java/com/mexazon/app/model/PostPhoto.java
+++ src/main/java/com/mexazon/app/model/PostPhoto.java
@@ -1,5 +1,21 @@
 package com.mexazon.app.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "post_photos")
 public class PostPhoto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long photoId;
+
+    @Column(name = "post_id", nullable = false)
+    private Long postId;
+
+    @Column(name = "photo_url", length = 255, nullable = false)
+    private String photoUrl;
+
+    @Column(name = "photo_order")
+    private Short photoOrder;
 }
