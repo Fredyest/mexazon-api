94c09c483a8d21b58f84684e048756f6149c5ce4
Alvaro Bazan <alvaarobj@gmail.com>
2025-10-16 20:08:33 -0600

    prueba porque no se si se guardo

--- src/main/java/com/mexazon/app/model/Business.java
+++ src/main/java/com/mexazon/app/model/Business.java
@@ -1,5 +1,37 @@
 package com.mexazon.app.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
 public class Business {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "business_id")
+    private Long businessId;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+	public Long getBusinessId() {
+		return businessId;
+	}
+
+	public void setBusinessId(Long businessId) {
+		this.businessId = businessId;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
 
 }
--- src/main/java/com/mexazon/app/model/BusinessHours.java
+++ src/main/java/com/mexazon/app/model/BusinessHours.java
@@ -1,5 +1,81 @@
 package com.mexazon.app.model;
+import java.time.LocalTime;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="business_hours")
 public class BusinessHours {
+
+    
+
+    public Business getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(Business business) {
+		this.business = business;
+	}
+
+	public BusinessHourId getId() {
+		return id;
+	}
+
+	public void setId(BusinessHourId id) {
+		this.id = id;
+	}
+
+	public LocalTime getTimeIn() {
+		return timeIn;
+	}
+
+	public void setTimeIn(LocalTime timeIn) {
+		this.timeIn = timeIn;
+	}
+
+	public LocalTime getTimeOut() {
+		return timeOut;
+	}
+
+	public void setTimeOut(LocalTime timeOut) {
+		this.timeOut = timeOut;
+	}
+
+	public boolean isWorking() {
+		return isWorking;
+	}
+
+	public void setWorking(boolean isWorking) {
+		this.isWorking = isWorking;
+	}
+
+	@ManyToOne
+    @MapsId("businessId") 
+    private Business business;
+
+    @EmbeddedId
+    private BusinessHourId id;
+
+    @Column(name = "time_in")
+    private LocalTime timeIn;
+
+    @Column(name = "time_out")
+    private LocalTime timeOut;
+
+    @Column(name = "is_working")
+    private boolean isWorking;
+
 
 }
