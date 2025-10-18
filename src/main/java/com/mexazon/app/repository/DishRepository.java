8b87aef44b5ddcb91c012d267d2c6883a13766f3
Alvaro Bazan <alvaarobj@gmail.com>
2025-10-17 21:22:04 -0600

    repository

--- src/main/java/com/mexazon/app/repository/DishRepository.java
+++ src/main/java/com/mexazon/app/repository/DishRepository.java
@@ -1,5 +1,14 @@
 package com.mexazon.app.repository;
 
-public class DishRepository {
+import com.mexazon.app.service.impl.Dish;
+import com.mexazon.app.service.impl.List;
 
+@Repository
+public interface DishRepository extends JpaRepository<Dish, Long> {
+
+	
+	List<Dish> findAllByBusiness_BusinessId(Long businessId);
+	
+	 List<Dish> findAllByCategory_CategoryId(Long categoryId);
+	 
 }
